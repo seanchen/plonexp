3eb8820d7820a02b93b4357209935dbb7a0ed031
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-02-19 13:56:35 +0000

    export from plonerm plonerm and import here

---
+++ iscorpio.plonerm/trunk/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='iscorpio.plonerm',
+      version=version,
+      description="Resource Management System on Plone Platform",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Python Zope Plone Resource Management',
+      author='iscorpio',
+      author_email='iscorpio@users.sourceforge.net',
+      url='http://plonerm.sourceforge.net',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['iscorpio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

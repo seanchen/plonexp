9cf575b335691dc00c4ed82c03eb31513e762139
Sean Chen <sean.chen@leocorn.com>
2009-02-19 19:39:05 +0000

    code clean up

--- iscorpio.plonepm/branches/ppm-upgrade2plone3/config.py
+++ iscorpio.plonepm/branches/ppm-upgrade2plone3/config.py
@@ -13,11 +13,3 @@ SKINS_DIR = "skins"
 # A global name space when this product is loaded into Python is 
 # preserved. It passes to the package_home() etc.
 GLOBALS = globals()
-
-# build journal project names, will be eliminated when the
-# configuration module is ready.
-BUILD_JOURNAL_PROJECT_NAMES = DisplayList ((
-        ('Express3.2-GR', 'Express 3.2 GeneralRelease'),
-        ('Express3.3-ML', 'Express 3.3 Multi Lender'),
-        ('Express3.3-BDN', 'Express 3.3 Bilangual Deal Notes'),
-        ))

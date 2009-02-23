ff8dbf2bfe375ad4e45cb06355493cf9df95211e
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-02-23 14:05:15 +0000

    let's get start now

---
+++ iscorpio.plonerm/trunk/iscorpio/plonerm/config.py
@@ -0,0 +1,13 @@
+
+# config.py
+
+# configuration constants for Plone Resource Management
+
+# project name we use this in somewhere like the permission name.
+PROJECTNAME = "PloneRM"
+
+# the skins folder.
+SKINS_DIR = "skins"
+
+# The global name space.
+GLOBALS = globals()

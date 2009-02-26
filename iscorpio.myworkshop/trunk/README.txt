7c1a8e46bae728c008b3485021c1bb8b2bf1e9eb
Sean Chen <sean.chen@leocorn.com>
2009-02-26 18:45:27 +0000

    let's get start to build myworkshop

---
+++ iscorpio.myworkshop/trunk/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+MyWorkshop will provide a Web service for none profit organizations to manage workshops, publish workshop events on a Web calendar, and manage attendees information and confirmation.
---
+++ iscorpio.myworkshop/trunk/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='iscoprio.myworkshop',
+      version=version,
+      description="online service for publishing and managing workshop info and event",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Python Zope Plone Event Calendar',
+      author='iScorpio',
+      author_email='iscorpio@users.sourceforge.net',
+      url='http://myworkshop.sourceforge.net',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['iscorpio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

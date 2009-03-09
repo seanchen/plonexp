3a7765952b3982a11628a7018bc22e9abefd6835
Sean Chen <sean.chen@leocorn.com>
2009-03-09 15:05:07 +0000

    the default project viw, using the zope 3 approach: browser and view

--- iscorpio.plonepm/trunk/iscorpio/plonepm/interfaces/__init__.py
+++ iscorpio.plonepm/trunk/iscorpio/plonepm/interfaces/__init__.py
@@ -7,3 +7,4 @@ __author__ = 'iScorpio <iscorpio@users.sourceforge.net>'
 __docformat__ = 'plaintext'
 
 
+from project import IPPMProject
---
+++ iscorpio.plonepm/trunk/iscorpio/plonepm/interfaces/project.py
@@ -0,0 +1,16 @@
+
+# project.py
+
+# all interfaces about project.
+
+__author__ = """iScorpio <iscorpio@users.sourceforge.net>"""
+__docformat__ = 'plaintext'
+
+from zope.interface import Interface
+
+class IPPMProject(Interface):
+    """ defines the interfaces for a project. empty for now.
+    """
+    pass
+
+# end of class IPPMProject

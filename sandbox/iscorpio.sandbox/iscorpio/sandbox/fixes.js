c94249bc0da4e8aed4eb051d711347c09c50177e
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-04-08 04:53:10 +0000

    javascript backup

---
+++ sandbox/iscorpio.sandbox/iscorpio/sandbox/fixes.js
@@ -0,0 +1,182 @@
+<table align="center"><tbody>
+    <tr>
+        <td valign="middle"><font size="2">Latest Fixes [<b><script type="text/javascript">document.write((new Date()).toDateString());</script></b>]: </font></td>
+        <td valign="middle">
+          <div id="fixesDiv"></div>
+        </td>
+    </tr>
+</tbody></table>
+
+  <!-- Add-On Settings -->
+  <script type="text/JavaScript">
+
+    /*******  Menu 0 Add-On Settings *******/
+    var b = qmad.qm1 = new Object();
+
+    // Box Animation Add On
+    b.box_animation_frames = 20;
+    b.box_accelerator = 0.4;
+    b.box_position = "center";
+
+    // Item Bullets (CSS - Imageless) Add On
+    b.ibcss_apply_to = "parent";
+    b.ibcss_main_type = "arrow-head-v";
+    b.ibcss_main_direction = "down";
+    b.ibcss_main_size = 5;
+    b.ibcss_main_bg_color = "transparent";
+    b.ibcss_main_border_color = "#555555";
+    b.ibcss_main_border_color_hover = "#dd3300";
+    b.ibcss_main_position_x = -19;
+    b.ibcss_main_position_y = -4;
+    b.ibcss_main_align_x = "right";
+    b.ibcss_main_align_y = "middle";
+    b.ibcss_sub_type = "arrow-head-v";
+    b.ibcss_sub_direction = "right";
+    b.ibcss_sub_size = 5;
+    b.ibcss_sub_bg_color = "transparent";
+    b.ibcss_sub_border_color = "#555555";
+    b.ibcss_sub_border_color_hover = "#dd3300";
+    b.ibcss_sub_position_x = -16;
+    b.ibcss_sub_align_x = "right";
+    b.ibcss_sub_align_y = "middle";
+
+  </script>
+<script type="text/javascript">
+wenjians = [{"bq":"ConaxSat (CNX)","wjs":[{"bq":"V1 (2007-12-10)","wj":"http://www.totalfta.com/updates/CNX_V1_4.46MTJC_up.zip"},{"bq":"Duo (2008-03-04)","wj":"http://www.totalfta.com/updates/0304_2120.zip"},{"bq":"Mini (2008-03-04)","wj":"http://www.totalfta.com/updates/0304_2120.zip"},{"bq":"Trio (2008-03-04)","wj":"http://www.totalfta.com/updates/0304_2120.zip"}]},{"bq":"ViewSat","wjs":[{"bq":"2000 PLATINUM & LITE (2008-03-05)","wj":"http://www.totalfta.com/updates/DBPSW-080305P.zip"},{"bq":"2000 Xtreme (2008-03-05)","wj":"http://www.totalfta.com/updates/DBPSW-080305X.zip"},{"bq":"Ultra (2008-03-05)","wj":"http://www.totalfta.com/updates/PFTAUSW-080305U.zip"},{"bq":"VS Pro (2008-03-05)","wj":"http://www.totalfta.com/updates/VSPRO-080305.zip"},{"bq":"7000 PVR (2008-03-05)","wj":"http://www.totalfta.com/updates/PVRSW_080305.zip"},{"bq":"9000 HD (2008-03-05)","wj":"http://www.totalfta.com/updates/PFTAHDSW-080305H.zip"}]},{"bq":"SonicView","wjs":[{"bq":"1000 (2008-03-04)","wj":"http://www.totalfta.com/updates/SV1000_V0304P%20CharlieBeverly%20Blackout%2003-04-08.zip"},{"bq":"PVR 1000 (2008-03-04)","wj":"http://www.totalfta.com/updates/PVR1000_V0192P%20CharlieBeverly%20Blackout%2003-04-08.zip"},{"bq":"4000 (2008-03-04)","wj":"http://www.totalfta.com/updates/SV4000_V0220P%20CharlieBeverly%20Blackout%2003-04-08.zip"},{"bq":"HD 8000 (2008-03-04)","wj":"http://www.totalfta.com/updates/HD8000_V0279P%20CharlieBeverly%20Blackout%2003-04-08.zip"}]},{"bq":"Pansat","wjs":[{"bq":"9200HD (2008-03-04)","wj":"http://203.169.142.72/~future/shine/9200/hd-9200bl_080304_9247_api.zip"},{"bq":"9000HD (2008-03-04)","wj":"http://203.169.142.72/~future/shine/9000/hd-9000bl_080304_9061_api.zip"},{"bq":"6000HXC (2008-03-04)","wj":"http://203.169.142.72/~future/shine/6000/x-6095bl_080304_3196_api.zip"},{"bq":"5000 (2008-03-04)","wj":"http://203.169.142.72/~future/shine/5000/x-95bl_080304_3165_api.zip"},{"bq":"250SM (2008-03-04)","wj":"http://203.169.142.72/~future/shine/250/x_250bl_080304_v582a_api.zip"},{"bq":"2800A (2008-03-04)","wj":"http://203.169.142.72/~future/shine/2800/x-88bl_080304_465eubx_api.zip"},{"bq":"2700A (2008-03-04)","wj":"http://203.169.142.72/~future/shine/2700/x-87bl_080304_b75_254_api.zip"},{"bq":"3500SD (2008-03-04)","wj":"http://203.169.142.72/~future/shine/3500/x-05bl_080304_370_api_nocard.zip"}]},{"bq":"Neosat","wjs":[{"bq":"IPRO 2000/Pro (2008-03-04)","wj":"http://www.futuredss.info/up_Ver280.rar"}]},{"bq":"Neusat","wjs":[{"bq":"Premium (2008-03-04)","wj":"http://www.futuredss.info/sp6033pre.rar"},{"bq":"HD (2008-03-04)","wj":"http://www.futuredss.info/neuhd_patch0014.zip"}]},{"bq":"coolsat","wjs":[{"bq":"5000 (2008-03-04)","wj":"http://www.box.net/shared/vq4mlyb44g"},{"bq":"6000 (2008-03-04)","wj":"http://www.box.net/shared/66oa8phbhj"},{"bq":"7000PVR (2008-03-04)","wj":"http://www.box.net/shared/h4yseayhch"},{"bq":"8000HD (2008-03-04)","wj":"http://www.box.net/shared/9r5c9y5vnm"}]}];
+
+function buildFixesList() {
+var rootUL = document.createElement("ul");rootUL.id = "qm1";rootUL.className = "qmmc";
+var topLI = document.createElement("li");var topHref = document.createElement("a");topHref.className = "qmparent";topHref.setAttribute("href", "javascript:void(0)");topHref.appendChild(document.createTextNode("Select Your FTA Receiver ..."));topLI.appendChild(topHref);
+var wenjiansUL = document.createElement("ul");
+for (var i = 0; i < wenjians.length; i ++) {    var wenJians = wenjians[i].wjs;
+    var wenjianLI = document.createElement("li");    var wenjianHref = document.createElement("a");    wenjianHref.setAttribute("href", "javascript:void(0)");    wenjianHref.appendChild(document.createTextNode(wenjians[i].bq));    wenjianLI.appendChild(wenjianHref);
+    var wjsUL = document.createElement("ul");    for (var j = 0; j < wenJians.length; j ++) {        var wjLI = document.createElement("li");        var wjHref = document.createElement("a");        wjHref.setAttribute("href", "javascript:void(0);");        wjHref.wz = wenJians[j].wj;        wjHref.onclick = xianShiWenjian;        wjHref.appendChild(document.createTextNode(wenJians[j].bq));        wjLI.appendChild(wjHref);
+        wjsUL.appendChild(wjLI);    }
+    wenjianLI.appendChild(wjsUL);
+    wenjiansUL.appendChild(wenjianLI);}
+topLI.appendChild(wenjiansUL);
+rootUL.appendChild(topLI);
+var clearLI = document.createElement("li");clearLI.className = "qmclear";clearLI.appendChild(document.createTextNode("&nbsp;"));
+rootUL.appendChild(clearLI);return rootUL;
+}
+
+function xianShiWenjian(event) {
+if (!event) event = window.event; var theSource = event.target ? event.target : event.srcElement;window.location = theSource.wz;
+}
+
+function buildLatestFixes() {
+var qm1 = buildFixesList();var fixesDiv = document.getElementById("fixesDiv");fixesDiv.appendChild(qm1);qm_create(1,false,0,500,false,false,false,false,false);
+}
+</script>
+
+<script type="text/javascript">buildLatestFixes();</script>
+
+<style type="text/css">
+.qmmc .qmdivider{display:block;font-size:1px;border-width:0px;border-style:solid;position:relative;z-index:1;}.qmmc .qmdividery{float:left;width:0px;}.qmmc .qmtitle{display:block;cursor:default;white-space:nowrap;position:relative;z-index:1;}.qmclear {font-size:1px;height:0px;width:0px;clear:left;line-height:0px;display:block;float:none !important;}.qmmc {position:relative;zoom:1;z-index:10;}.qmmc a, .qmmc li {float:left;display:block;white-space:nowrap;position:relative;z-index:1;}.qmmc div a, .qmmc ul a, .qmmc ul li {float:none;}.qmsh div a {float:left;}.qmmc div{visibility:hidden;position:absolute;}.qmmc li {z-index:auto;}.qmmc ul {left:-10000px;position:absolute;z-index:10;}.qmmc, .qmmc ul {list-style:none;padding:0px;margin:0px;}.qmmc li a {float:none}.qmmc li:hover>ul{left:auto;}#qm1 ul {top:100%;}#qm1 ul li:hover>ul{top:0px;left:100%;}
+  #qm1  
+  { 
+    width:auto;
+    background-color:transparent;
+  }
+  #qm1 a  
+  { 
+    padding:5px 40px 5px 8px;
+    background-color:#ffffff;
+    color:#000000;
+    font-family:Arial;
+    font-size:11px;
+    text-decoration:none;
+    border-width:1px;
+    border-style:solid;
+    border-color:#dd3300;
+  }
+  #qm1 a:hover  
+  { 
+    background-color:#fcebcd;
+  }
+  #qm1 li:hover>a 
+  { 
+    background-color:#fcebcd;
+  }
+  body #qm1 .qmactive, body #qm1 .qmactive:hover  
+  { 
+    background-color:#f9d08c;
+    text-decoration:underline;
+  }
+  #qm1 div, #qm1 ul 
+  { 
+    padding:5px;
+    margin:-1px 0px 0px;
+    background-color:#fcebcd;
+    border-width:1px;
+    border-style:solid;
+    border-color:#dd3300;
+  }
+  #qm1 div a, #qm1 ul a 
+  { 
+    padding:2px 40px 2px 5px;
+    background-color:transparent;
+    border-width:0px;
+    border-style:none;
+    border-color:#000000;
+  }
+  #qm1 div a:hover  
+  { 
+    background-color:transparent;
+    color:#dd3300;
+    text-decoration:underline;
+  }
+  #qm1 ul li:hover>a  
+  { 
+    background-color:transparent;
+    color:#dd3300;
+    text-decoration:underline;
+  }
+  body #qm1 div .qmactive, body #qm1 div .qmactive:hover  
+  { 
+    background-color:transparent;
+    color:#dd3300;
+  }
+  #qm1 .qmtitle 
+  { 
+    cursor:default;
+    padding:3px 0px 3px 4px;
+    color:#444444;
+    font-family:arial;
+    font-size:11px;
+    font-weight:bold;
+  }
+  #qm1 .qmdividerx  
+  { 
+    border-top-width:1px;
+    margin:4px 0px;
+    border-color:#bfbfbf;
+  }
+  #qm1 .qmdividery  
+  { 
+    border-left-width:1px;
+    height:15px;
+    margin:4px 2px 0px;
+    border-color:#aaaaaa;
+  }
+  ul#qm1 .qmparent  
+  { 
+    background-image:url(qmimages/cssalt1_arrow_down.gif);
+    background-repeat:no-repeat;
+    background-position:97% 50%;
+  }
+  ul#qm1 li:hover > a.qmparent  
+  { 
+    background-image:url(qmimages/cssalt1_arrow_down_hover.gif);
+    text-decoration:underline;
+  }
+  ul#qm1 ul .qmparent 
+  { 
+    background-image:url(qmimages/cssalt1_arrow_right.gif);
+  }
+  ul#qm1 ul li:hover > a.qmparent 
+  { 
+    background-image:url(qmimages/cssalt1_arrow_right_hover.gif);
+  }
+.qmfv{visibility:visible !important;}.qmfh{visibility:hidden !important;}
+</style>

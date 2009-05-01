bd3f630bad737ab4625028ec5d19ed2135dab2a9
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-05-01 19:20:51 +0000

    more testing code.

--- sandbox/iscorpio.sandbox/iscorpio/sandbox/first.py
+++ sandbox/iscorpio.sandbox/iscorpio/sandbox/first.py
@@ -18,19 +18,19 @@ if __name__ == "__main__":
     size = len(os.listdir(SPOOL_FOLDER))
     if size > 0:
 
-    timestamp = time.strftime('%Y%m%d-%H%M%S')
-    #output = commands.getoutput('scp %s/* user@10.68.6.159:~/spool' % SPOOL_FOLDER)
-    output = os.system('scp %s/* user@10.68.6.159:~/spool' % SPOOL_FOLDER)
-
-    logFile = open('%s/log' % BACKUP_FOLDER, 'a')
-    logFile.write('\n---------------- Started at: %s' % timestamp)
-    logFile.write('\nCopied %d file(s)' % size)
-    logFile.write('\n------------------------------------\n')
-    logFile.close()
-
-    commands.getoutput("mkdir %s/%s" % (BACKUP_FOLDER, timestamp))
-    commands.getoutput("mv %s/* %s/%s" %
-                       (SPOOL_FOLDER, BACKUP_FOLDER, timestamp))
+        timestamp = time.strftime('%Y%m%d-%H%M%S')
+        #output = commands.getoutput('scp %s/* user@10.68.6.159:~/spool' % SPOOL_FOLDER)
+        output = os.system('scp %s/* user@10.68.6.159:~/spool' % SPOOL_FOLDER)
+    
+        logFile = open('%s/log' % BACKUP_FOLDER, 'a')
+        logFile.write('\n---------------- Started at: %s' % timestamp)
+        logFile.write('\nCopied %d file(s)' % size)
+        logFile.write('\n------------------------------------\n')
+        logFile.close()
+    
+        commands.getoutput("mkdir %s/%s" % (BACKUP_FOLDER, timestamp))
+        commands.getoutput("mv %s/* %s/%s" %
+                           (SPOOL_FOLDER, BACKUP_FOLDER, timestamp))
 
 
 import smtplib

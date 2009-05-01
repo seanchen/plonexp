c259d0da837ceed61273851b7d5059a70660df03
Sean Chen <sean.chen@leocorn.com>
2009-05-01 19:20:51 +0000

    more testing code.

---
+++ sandbox/iscorpio.sandbox/iscorpio/sandbox/tutu.py
@@ -0,0 +1,219 @@
+# tutu.py
+
+# a mailing list services.
+
+import re
+import email
+import imaplib
+import smtplib
+
+IMAP_MAILHOST = 'imap.gmail.com'
+IMAP_PORT = 993
+SMTP_MAILHOST = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+class EmailServices(object):
+
+    """ ImapServices will provide a easy and simple interface to
+    access IMAP email services.
+    """
+
+    # init.
+    def __init__(self):
+
+        # for IMAP server.
+        self.imapSession = None
+        # for SMTP server.
+        self.smtpSession = None
+
+    # start a imap session.
+    def startImapSession(self, username, password):
+
+        self.imapSession = imaplib.IMAP4_SSL(IMAP_MAILHOST, IMAP_PORT)
+        return self.imapSession.login(username, password)
+
+    # setart a SMTP session
+    def startSmtpSession(self, username, password):
+
+        self.smtpSession = smtplib.SMTP(SMTP_MAILHOST, SMTP_PORT)
+        # gmail using tls for smtp.
+        self.smtpSession.ehlo()
+        self.smtpSession.starttls()
+        self.smtpSession.ehlo()
+        self.smtpSession.login(username, password)
+        return self.smtpSession
+
+    # end a imap session.
+    def closeImapSession(self):
+
+        if self.imapSession:
+            self.imapSession.close()
+            self.imapSession.logout()
+            self.imapSession = None
+
+    # quit the SMTP session
+    def quitSmtpSession(self):
+
+        if self.smtpSession:
+            self.smtpSession.quit()
+            self.smtpSession = None
+
+    # return all selectable mailboxes
+    def getAllMailboxes(self):
+
+        result, response = self.imapSession.list()
+
+        mailboxes = []
+        if result == 'OK':
+            for each in response:
+                # suppose the response format is somethin like
+                # (\Noselect) "/" "[Gmail]"
+                if each.find('\\Noselect') >= 0:
+                    continue
+
+                folders = re.findall('"(.*)"', each)[0].split('" "')
+                mailboxes.append(folders[1])
+
+        return mailboxes
+
+    # get the new messages in a list:
+    # each message will be an instance of email.message.Message
+    def getNewMessages(self, mailbox='INBOX'):
+
+        # assume we check the new message from INBOX
+        result, response = self.imapSession.select(mailbox)
+        #print response
+        # we are in select state now.
+        result, response = self.imapSession.search(None, 'UNSEEN')
+        # response should be a string of message number
+        #print response
+        msgs=[]
+        for sequence in response[0].split():
+            # get the message and
+            result, message = self.imapSession.fetch(sequence, '(RFC822)')
+            #print "===================================="
+            #print message[0]
+            emailMessage = email.message_from_string(message[0][1])
+            msgs.append(emailMessage)
+
+        return msgs
+
+    # return non flagged sent mails
+    def getNotFlaggedOutMail(self):
+
+        self.imapSession.select('[Gmail]/Sent Mail')
+
+        typ, msgnums = self.imapSession.search(None, 'NOT', 'FLAGGED')
+
+        for one in msgnums[0].split():
+            # fetch each message
+            typ, data = self.imapSession.fetch(one, '(RFC822)')
+            # print out the message body.
+            print 'Message %s\n%s\n' % (one, data[0][1])
+
+    # send message,
+    def sendMail(self, fromAddr, toAddr, messageString):
+
+        if self.smtpSession == None:
+
+            return "SMTP Session NOT start!"
+
+        self.smtpSession.sendmail(fromAddr, toAddr, messageString)
+
+
+class EmailRepository(object):
+
+    """
+    """
+
+    # init
+    # fileName should be a full path name.
+    def __init__(self, fileName):
+
+        self.repoFileName = fileName
+        self.fileNewLine = None
+        # this dict is mainly for maintainence purpose.
+        self.emailDict = {}
+        # tag as the key and the value is a list of full email
+        self.tagDict = {}
+
+        self.loadEmails(self.repoFileName)
+
+    # load emails, full names, tags from the given file.
+    def loadEmails(self, fileName):
+
+        # load the file content into memory.
+        repoFile = open(fileName)
+        try:
+            for line in repoFile:
+                fullEmail, tags = line.split(';')
+                # get name and email separatly
+                fullName, email = fullEmail.replace('>', '').split(' <')
+                # preparing the taglist.
+                tagList = tags.split(',')
+                # pop out the new line, which is the last one.
+                fileNewLine = tagList.pop()
+                if self.fileNewLine == None:
+                    self.fileNewLine = fileNewLine
+                # build the email dict.
+                self.emailDict[email] = (fullName, tagList)
+
+                # build the tag dict, make sure everybody has tag All.
+                if not tagList.__contains__('All'):
+                    tagList.append('All')
+                for tag in tagList:
+                    if not self.tagDict.has_key(tag):
+                        self.tagDict[tag] = []
+                    self.tagDict[tag].append(fullEmail)
+        finally:
+            repoFile.close()
+
+    # return the full email as a list for the given tag.
+    def getEmails(self, tagName='All'):
+
+        return self.tagDict[tagName]
+
+    # add tag to a email address.
+    # tags should be a string with ',' as separator.  Its default value
+    # will be 'All'
+    def tagEmail(self, email, tags='All', fullName=None):
+
+        newTagList = tags.split(',')
+        if fullName == None:
+            fullName = email.split('@')[0]
+        if self.emailDict.has_key(email):
+            fullName, tagList = self.emailDict[email]
+            newTagList.extend(tagList)
+            newTagList = list(set(newTagList))
+
+        self.emailDict[email] = (fullName, newTagList)
+
+    # remove email, normally this is a bad email: could not deliver.
+    def removeEmails(self, emailsList):
+
+        for email in emailsList:
+            self.emailDict.pop(email, None)
+
+    # remove tags for the given email.
+    def removeEmailsTags(self, emailsList, tags):
+
+        removeTagList = tags.split(',')
+        for email in emailsList:
+            if self.emailDict.has_key(email):
+                fullName, tagList = self.emailDict[email]
+                for tag in removeTagList:
+                    tagList.remove(tag)
+                self.emailDict[email] = (fullName, tagList)
+            else:
+                self.tagEmail(email)
+
+    # save the emails to file.
+    def saveEmails(self):
+
+        fileRepo = open(self.repoFileName, 'w')
+        for email, nameTags in self.emailDict.iteritems():
+            tags = ','.join((','.join(nameTags[1]), self.fileNewLine))
+            fileRepo.write('%s <%s>;%s' % (nameTags[0], email, tags))
+
+        fileRepo.flush()
+        fileRepo.close()

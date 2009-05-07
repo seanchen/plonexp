3028cf9187a3bfe6d5086caa988d66cc2bfa2365
Sean Chen <sean.chen@leocorn.com>
2009-05-07 18:54:53 +0000

    adding blogger services

--- sandbox/iscorpio.sandbox/setup.py
+++ sandbox/iscorpio.sandbox/setup.py
@@ -24,6 +24,7 @@ setup(name = 'iscorpio.sandbox',
       zip_safe = False,
       install_requires = [
         'setuptools',
+        'gdata',
         # -*- Extra requirements: -*-
       ],
       entry_points = """

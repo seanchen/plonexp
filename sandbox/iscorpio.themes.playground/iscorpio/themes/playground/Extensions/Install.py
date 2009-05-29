8c749a9b0b44afc1719b37ba0376065ceec6cc7e
Sean Chen <sean.chen@leocorn.com>
2009-05-29 16:24:05 +0000

    add the uninstall profile to cleanly remove everthing when we uninstall this theme.

---
+++ sandbox/iscorpio.themes.playground/iscorpio/themes/playground/Extensions/Install.py
@@ -0,0 +1,11 @@
+from Products.CMFCore.utils import getToolByName
+
+def install(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-iscorpio.themes.playground:default')
+    return "Ran all import steps."
+
+def uninstall(portal):
+    setup_tool = getToolByName(portal, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-iscorpio.themes.playground:uninstall')
+    return "Ran all uninstall steps."

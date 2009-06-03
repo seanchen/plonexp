d9163321789c1516ed5b2f6a3c95ce0d1c5f0f1f
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-06-03 18:51:57 +0000

    Customizing the logo for the theme Iscorpio Themes Playground. define the theme in browser/configure.zcml, and used it in both default and uninstall GenericSetup profiles

--- sandbox/iscorpio.themes.playground/iscorpio/themes/playground/browser/viewlets.py
+++ sandbox/iscorpio.themes.playground/iscorpio/themes/playground/browser/viewlets.py
@@ -1,3 +1,13 @@
+from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
+class MyLogoViewlet(ViewletBase):
+
+    render = ViewPageTemplateFile('templates/mylogo.pt')
+
+    def update(self):
+
+        portal_state = getMultiAdapter((self.context, self.request),
+                                       name=u'plone_portal_state')
+        self.sitehome = portal_state.navigation_root_url()

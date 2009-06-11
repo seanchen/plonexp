2da32a7295f608805081ccf96274d47714668c41
Sean Chen <sean.chen@leocorn.com>
2009-06-11 18:01:06 +0000

    added an adapter class, so we can use the CMF Tool iscorpio_plonelab to save configuration item's value.  It's value will persist after restart the zope server.

--- sandbox/iscorpio.plonelab/iscorpio/plonelab/browser/config.py
+++ sandbox/iscorpio.plonelab/iscorpio/plonelab/browser/config.py
@@ -1,12 +1,17 @@
 # config.py
 
 from zope.component import getUtility
+from zope.component import adapts
 from zope.interface import implements
 from zope.formlib.form import FormFields
 from zope.schema.fieldproperty import FieldProperty
 from zope.i18nmessageid import MessageFactory
 
 from OFS.SimpleItem import SimpleItem
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 from plone.app.controlpanel.form import ControlPanelForm
 
@@ -24,6 +29,26 @@ class SillyConfigurationForm(ControlPanelForm):
 
     form_name = _(u'Section Name',
                   default=u'Attributes')
+
+class SillyConfigurationAdapter(SchemaAdapterBase):
+
+    adapts(IPloneSiteRoot)
+    implements(ISillyConfiguration)
+
+    def __init__(self, context):
+        super(SillyConfigurationAdapter, self).__init__(context)
+        self.tool = getToolByName(self.context,
+                                  'iscorpio_plonelab')
+
+    def get_favorite_color(self):
+
+        return self.tool.favorite_color
+
+    def set_favorite_color(self, value):
+
+        self.tool.favorite_color = value
+
+    favorite_color = property(get_favorite_color, set_favorite_color)
 
 # the utility class to do storage.
 class SillyConfiguration(SimpleItem):

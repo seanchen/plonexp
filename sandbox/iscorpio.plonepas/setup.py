3d0b2b9b6ce5964c4c7331b6ba05d466cefea003
chyxiang <chyxiang@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-08-19 17:31:03 +0000

    new playgroud for try out Plone PAS. the empty package generated from paster on basic namespace template.

---
+++ sandbox/iscorpio.plonepas/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='iscorpio.plonepas',
+      version=version,
+      description="Plone PAS Playground",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Python Zope Plone PAS',
+      author='Sean Chen',
+      author_email='chyxiang@gmail.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['iscorpio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

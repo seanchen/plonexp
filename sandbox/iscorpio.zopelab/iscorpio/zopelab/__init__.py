63cc77c58bebdd1994ea3b412acd05cae89b033f
chyxiang <chyxiang@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-08-22 02:17:04 +0000

    A simplest Zope product, which has hardcoded id and has NO constructor form. By using GenericSetup, we have to specify the initialize method in file configure.zcml.

---
+++ sandbox/iscorpio.zopelab/iscorpio/zopelab/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py
+
+"""
+The initialize method will seat here. This method will be invoked
+when Zope server start up.
+"""
+
+from one import simple
+
+# zope initialize method.
+def initialize(context):
+    """
+    context will be passed from Zope server
+    """
+
+    context.registerClass(
+        simple.SimpleProduct,
+        constructors = (
+            simple.manage_addSimpleProduct,
+        )
+    )

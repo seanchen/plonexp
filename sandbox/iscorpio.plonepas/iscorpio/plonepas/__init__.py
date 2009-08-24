9cadd0ab3bfb7ee94394dd544128d66ecea6e937
Sean Chen <sean.chen@leocorn.com>
2009-08-24 16:21:25 +0000

    a simple PAS plugin to redirect authentication challenge to google homepage. 1. add it on ZMI acl_users folder, 2. set up challenge plugin configuration to active it.

--- sandbox/iscorpio.plonepas/iscorpio/plonepas/__init__.py
+++ sandbox/iscorpio.plonepas/iscorpio/plonepas/__init__.py
@@ -1,6 +1,36 @@
 # __init__.py
 
+"""
+Zope server will load this file when starts up. Initialization work
+happens here.
+"""
+
+from Products.PluggableAuthService import registerMultiPlugin
+
+from plugins import challenge
+
+__author__ = "Sean Chen"
+__email__ = "chyxiang@gmail.com"
+
+# try to register RedirectChallenge available in add plugin selection
+# dropdown list.
+try:
+    registerMultiPlugin(challenge.RedirectChallenge.meta_type)
+except RuntimeError:
+    # ignore exceptions on re-registering the plugin
+    pass
+
+# Zope server will load this method when starts up.
 def initialize(context):
     """
     Initializer called when used as a Zope 2 product.
     """
+
+    context.registerClass(
+        challenge.RedirectChallenge,
+        constructors=(
+            challenge.manage_addRedirectChallengeForm,
+            challenge.manage_addRedirectChallenge,
+        ),
+        visibility=None
+    )

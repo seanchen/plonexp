e54e6b09038cbc9f9ed58e8e8967a4a97a8d36cd
Sean Chen <sean.chen@leocorn.com>
2009-09-11 19:03:09 +0000

    the basic parts for a Zope product.

---
+++ sandbox/iscorpio.zopelab/iscorpio/zopelab/dove/nest.py
@@ -0,0 +1,43 @@
+# nest.py
+
+"""
+a simple Zope product trying to use GenericSetup to do some initialization
+work.
+"""
+
+from OFS import SimpleItem
+from Globals import InitializeClass
+
+__author__ = "Sean Chen"
+__email__ = "chyxiang@gmail.com"
+
+# CMFDove product class. Extends from SimpleItem to get some basic
+# behavior for work with ZMI
+class CMFDove(SimpleItem.SimpleItem):
+    """
+    A simple Zope product leverage on GenericSetup.
+    """
+
+    # specify the meta type to show up on the add drop down for a Zope
+    # instance.
+    meta_type = "iScorpio ZopeLab Dove"
+
+    # __init__
+    def __init__(self, id, title=''):
+        """
+        the initialization.
+        """
+
+        self.id = id
+        self.title = title
+
+    # the default home page for this produce.
+    def index_html(self):
+        """
+        Just try to say something here.
+        """
+
+        return """<html><body>Welcome to %s</body></html>
+        """ % self.title
+
+InitializeClass(CMFDove)

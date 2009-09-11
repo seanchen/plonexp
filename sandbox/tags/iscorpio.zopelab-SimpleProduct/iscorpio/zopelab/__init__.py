9461fe72416c21a6196d2883d08176e56970f38e
Sean Chen <sean.chen@leocorn.com>
2009-09-11 15:41:24 +0000

    tag for the simplest Zope Product

---
+++ sandbox/tags/iscorpio.zopelab-SimpleProduct/iscorpio/zopelab/__init__.py
@@ -0,0 +1,22 @@
+# __init__.py
+
+"""
+The initialize method will seat here. This method will be invoked
+when Zope server start up.
+"""
+
+from one import simple
+
+# zope initialize method.
+def initialize(context):
+    """
+    context will be passed from Zope server
+    """
+
+    context.registerClass(
+        simple.SimpleProduct,
+        constructors = (
+            simple.manage_addSimpleProductForm,
+            simple.manage_addSimpleProduct,
+        )
+    )

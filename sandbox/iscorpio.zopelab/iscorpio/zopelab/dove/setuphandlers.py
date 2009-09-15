f3f38ffbf555ef42528f2a0bda8138c33c0d0cc0
chyxiang <chyxiang@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-09-15 16:17:20 +0000

    Now when we create the CMF Dove product, we also import the gs profiles. this just try to prove the concept by using setup tool to import steps from a profile: 1. Dove has to be created in a folder of Zope root 2. There is nothing in the GS profile.

---
+++ sandbox/iscorpio.zopelab/iscorpio/zopelab/dove/setuphandlers.py
@@ -0,0 +1,11 @@
+# sethandlers.py
+
+
+# trying to import various
+def importVarious(context):
+    
+    if context.readDataFile('dove.various.txt') is None:
+        return
+
+    portal = context.getSite()
+    #setup_localrole_plugin(portal)

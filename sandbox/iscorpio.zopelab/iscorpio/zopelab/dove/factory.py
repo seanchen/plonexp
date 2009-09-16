1540acbb3986a183acfe1ebeb060b17217d37e8c
chyxiang <chyxiang@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-09-16 19:50:58 +0000

    added the generic setup tool for dove folder. and then import the dove gs profile. however dove gs profile is not a base profile, so the dove setup Import tab will not work unless we select a basline profile from Profile tab. The error we got for Import Tab is KeyError: 'Unknown context ""'

--- sandbox/iscorpio.zopelab/iscorpio/zopelab/dove/factory.py
+++ sandbox/iscorpio.zopelab/iscorpio/zopelab/dove/factory.py
@@ -7,10 +7,14 @@ instance of CMFDove.
 
 from Acquisition import aq_inner, aq_parent
 from Globals import DTMLFile
+from Products.GenericSetup.tool import SetupTool
 from nest import CMFDove
 
 __author__ = "Sean Chen"
 __email__ = "chyxiang@gmail.com"
+
+_SETUP_TOOL_ID = 'dove_setup'
+_DOVE_PROFILE_ID = 'iscorpio.zopelab.dove:dove'
 
 # the intialize form.
 manage_addDoveForm = DTMLFile('www/addCMFDove', globals())
@@ -24,8 +28,12 @@ def manage_addDove(self, id, title='', REQUEST=None):
     self._setObject(id, CMFDove(id, title))
 
     dove = self._getOb(id)
+    # add generic set up tool for dove.
+    dove._setObject(_SETUP_TOOL_ID, SetupTool(_SETUP_TOOL_ID))
+    setup_tool = getattr(dove, _SETUP_TOOL_ID)
     #parent = aq_parent(aq_inner(self))
     #parent.Plone.portal_setup.runAllImportStepsFromProfile('profile-iscorpio.zopelab.dove:dove')
+    setup_tool.runAllImportStepsFromProfile('profile-%s' % _DOVE_PROFILE_ID)
 
     if REQUEST:
         REQUEST['RESPONSE'].redirect('%s/index_html' % dove.absolute_url(),

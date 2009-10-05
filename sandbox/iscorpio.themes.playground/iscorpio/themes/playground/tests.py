38ed71a960d0689799eb74b00549a3841c21abd8
Sean Chen <sean.chen@leocorn.com>
2009-10-05 03:38:22 +0000

    added the simple unit test cases for the simple portlet.  It only has 2 easy test cases.  We hided the assignment for context since it has hard-coded object path.  We nee create some dummy data for unit testin next time.

--- sandbox/iscorpio.themes.playground/iscorpio/themes/playground/tests.py
+++ sandbox/iscorpio.themes.playground/iscorpio/themes/playground/tests.py
@@ -1,53 +1,71 @@
+
+# tests.py
+
+"""
+The unit test cases for iscorpio thems playground
+"""
+
 import unittest
 
 from zope.testing import doctestunit
 from zope.component import testing
+from zope.component import getUtility
+from zope.app.component.hooks import setHooks, setSite
 from Testing import ZopeTestCase as ztc
 
+from plone.portlets.interfaces import IPortletType
+
+from Products.GenericSetup.utils import _getDottedName
 from Products.Five import zcml
 from Products.Five import fiveconfigure
+
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
-ptc.setupPloneSite()
+from Products.PloneTestCase.layer import onsetup
 
 import iscorpio.themes.playground
 
-class TestCase(ptc.PloneTestCase):
-    class layer(PloneSite):
-        @classmethod
-        def setUp(cls):
-            fiveconfigure.debug_mode = True
-            ztc.installPackage(iscorpio.playground)
-            fiveconfigure.debug_mode = False
+__author__ = "Sean Chen"
+__email__ = "sean.chen@leocorn.com"
 
-        @classmethod
-        def tearDown(cls):
-            pass
+ptc.setupPloneSite(extension_profiles=['iscorpio.themes.playground:default'])
 
+@onsetup
+def setup_product():
+
+    fiveconfigure.debug_mode = True
+    zcml.load_config('configures.zcml', iscorpio.themes.playground)
+    fiveconfigure.debug_mode = True
+    ztc.installProduct('iscorpio.themes.playground')
+
+class PlaygroundTestCase(ptc.PloneTestCase):
+    """
+    base for test cases.
+    """
+
+class TestSimplePortlet(PlaygroundTestCase):
+
+    def afterSetUp(self):
+        setHooks()
+        setSite(self.portal)
+
+    def testPortletTypeRegistered(self):
+        portlet = getUtility(IPortletType, name='iscorpio.themes.playground.portlet.Simple')
+        self.assertEquals(portlet.addview, 'iscorpio.themes.playground.portlet.Simple')
+
+    def testRegisteredInterfaces(self):
+        portlet = getUtility(IPortletType, name='iscorpio.themes.playground.portlet.Simple')
+        registered_interfaces = [_getDottedName(i) for i in portlet.for_]
+        registered_interfaces.sort()
+        self.assertEquals(['plone.app.portlets.interfaces.IColumn',
+                           'plone.app.portlets.interfaces.IDashboard'],
+                          registered_interfaces)
 
 def test_suite():
-    return unittest.TestSuite([
 
-        # Unit tests
-        #doctestunit.DocFileSuite(
-        #    'README.txt', package='iscorpio.playground',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-        #doctestunit.DocTestSuite(
-        #    module='iscorpio.playground.mymodule',
-        #    setUp=testing.setUp, tearDown=testing.tearDown),
-
-
-        # Integration tests that use PloneTestCase
-        #ztc.ZopeDocFileSuite(
-        #    'README.txt', package='iscorpio.playground',
-        #    test_class=TestCase),
-
-        #ztc.FunctionalDocFileSuite(
-        #    'browser.txt', package='iscorpio.playground',
-        #    test_class=TestCase),
-
-        ])
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSimplePortlet))
+    return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')

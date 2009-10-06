9317dd0c569271a5b78c61d7539e46745d2b3fb3
Sean Chen <sean.chen@leocorn.com>
2009-10-06 13:56:04 +0000

    added the test case for activating plugins.

--- sandbox/iscorpio.plonepas/iscorpio/plonepas/tests/testSqurrel.py
+++ sandbox/iscorpio.plonepas/iscorpio/plonepas/tests/testSqurrel.py
@@ -7,6 +7,12 @@ test cases for squirrel multi plugins.
 
 import unittest
 
+from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
+from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin
+from Products.PluggableAuthService.interfaces.plugins import IPropertiesPlugin
+from Products.PluggableAuthService.interfaces.plugins import IExtractionPlugin
+from Products.PluggableAuthService.interfaces.plugins import ICredentialsUpdatePlugin
+
 from base import IscorpioPASTestCase
 
 __author__ = "Sean Chen"
@@ -23,6 +29,33 @@ class SquirrelTestCase(IscorpioPASTestCase):
         self.assertEquals(self.acl_users.\
                           objectIds(['iScorpio PlonePAS Squirrel Plugins']),
                           ['squirrel'])
+
+    # test activate the plugins.
+    def testActivateSquirrel(self):
+        iscorpio = self.acl_users.manage_addProduct['iscorpio.plonepas']
+        iscorpio.manage_addSquirrelPlugins('squirrel')
+
+        plugins = self.acl_users.plugins
+
+        plugins.activatePlugin(IAuthenticationPlugin, 'squirrel')
+        found = plugins._getPlugins(IAuthenticationPlugin)
+        self.assertTrue('squirrel' in found)
+
+        plugins.activatePlugin(IUserEnumerationPlugin, 'squirrel')
+        found = plugins._getPlugins(IUserEnumerationPlugin)
+        self.assertTrue('squirrel' in found)
+
+        plugins.activatePlugin(IPropertiesPlugin, 'squirrel')
+        found = plugins._getPlugins(IPropertiesPlugin)
+        self.assertTrue('squirrel' in found)
+
+        plugins.activatePlugin(IExtractionPlugin, 'squirrel')
+        found = plugins._getPlugins(IExtractionPlugin)
+        self.assertTrue('squirrel' in found)
+
+        plugins.activatePlugin(ICredentialsUpdatePlugin, 'squirrel')
+        found = plugins._getPlugins(ICredentialsUpdatePlugin)
+        self.assertTrue('squirrel' in found)
 
 def test_suite():
     suite = unittest.TestSuite()

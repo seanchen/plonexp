28af8e0a4fff40ac9ec45c3aa5352ae8b2b6462e
Sean Chen <sean.chen@leocorn.com>
2009-10-07 13:08:49 +0000

    playing with available property and unit testing: 1. using available property to bind simple portlet with News Item only. 2. add test cases to test renderer and available property 3. created a dummy news item for testing.

--- sandbox/iscorpio.themes.playground/iscorpio/themes/playground/tests/testSimplePortlet.py
+++ sandbox/iscorpio.themes.playground/iscorpio/themes/playground/tests/testSimplePortlet.py
@@ -8,9 +8,12 @@ Here we testing the simple portlet.
 import unittest
 
 from zope.component import getUtility
+from zope.component import getMultiAdapter
 
+from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletType
 from plone.portlets.interfaces import IPortletAssignment
+from plone.portlets.interfaces import IPortletRenderer
 from plone.portlets.interfaces import IPortletDataProvider
 
 from Products.GenericSetup.utils import _getDottedName
@@ -26,11 +29,14 @@ __email__ = "sean.chen@leocorn.com"
 class TestSimplePortlet(PlaygroundTestCase):
 
     def testPortletTypeRegistered(self):
-        portlet = getUtility(IPortletType, name='iscorpio.themes.playground.portlet.Simple')
-        self.assertEquals(portlet.addview, 'iscorpio.themes.playground.portlet.Simple')
+        portlet = getUtility(IPortletType,
+                             name='iscorpio.themes.playground.portlet.Simple')
+        self.assertEquals(portlet.addview,
+                          'iscorpio.themes.playground.portlet.Simple')
 
     def testRegisteredInterfaces(self):
-        portlet = getUtility(IPortletType, name='iscorpio.themes.playground.portlet.Simple')
+        portlet = getUtility(IPortletType,
+                             name='iscorpio.themes.playground.portlet.Simple')
         registered_interfaces = [_getDottedName(i) for i in portlet.for_]
         registered_interfaces.sort()
         self.assertEquals(['plone.app.portlets.interfaces.IColumn',
@@ -42,10 +48,46 @@ class TestSimplePortlet(PlaygroundTestCase):
         self.failUnless(IPortletAssignment.providedBy(portlet))
         self.failUnless(IPortletDataProvider.providedBy(portlet))
 
+    def testRenderer(self):
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.leftcolumn',
+                             context=self.portal)
+        assignment = simple.SimpleAssignment()
+
+        renderer = getMultiAdapter((context, request, view, manager,
+                                    assignment),
+                                   IPortletRenderer)
+        self.failUnless(renderer.available is False)
+        self.failUnless(isinstance(renderer, simple.SimpleRenderer))
+
+class TestRenderer(PlaygroundTestCase):
+
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
+        context = context or self.portal
+        request = request or self.app.REQUEST
+        view = view or self.portal.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.leftcolumn',
+                             context=self.portal)
+        assignment = simple.SimpleAssignment()
+
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
+
+    def testAvailable4News(self):
+
+        self.portal.invokeFactory('News Item', 'news1')
+        newsItem = getattr(self.portal, 'news1')
+        r = self.renderer(context=newsItem)
+        self.failUnless(r.available is True)
+
 def test_suite():
 
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestSimplePortlet))
+    suite.addTest(unittest.makeSuite(TestRenderer))
     return suite
 
 if __name__ == '__main__':

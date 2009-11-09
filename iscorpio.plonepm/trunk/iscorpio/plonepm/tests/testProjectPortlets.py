5f252ffaa3532d1c694dcb05492453a0f6f078f6
Sean Chen <sean.chen@leocorn.com>
2009-11-09 14:33:49 +0000

    adding the recent changed artifacts portlet. this is the first version, we will make it better by adding configurable features later.

--- iscorpio.plonepm/trunk/iscorpio/plonepm/tests/testProjectPortlets.py
+++ iscorpio.plonepm/trunk/iscorpio/plonepm/tests/testProjectPortlets.py
@@ -19,6 +19,7 @@ from plone.portlets.interfaces import IPortletDataProvider
 from Products.GenericSetup.utils import _getDottedName
 
 from iscorpio.plonepm.portlets import projectSimpleNav
+from iscorpio.plonepm.portlets import recentArtifacts
 
 from base import PlonepmTestCase
 
@@ -157,10 +158,70 @@ class TestProjectSimpleNavRenderer(PlonepmTestCase):
         self.assertEquals(stories[2]['url'], story1.absolute_url())
         self.assertEquals(stories[2]['title'], 'story1')
 
+class TestRecentArtifactsPortlet(PlonepmTestCase):
+
+    def testRenderer(self):
+
+        context = self.folder
+        request = self.folder.REQUEST
+        view = self.folder.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.leftcolumn',
+                             context=self.portal)
+        assignment = recentArtifacts.Assignment()
+        renderer = getMultiAdapter((context, request, view, manager, assignment),
+                                   IPortletRenderer)
+        self.failIf(renderer.available)
+        self.failUnless(isinstance(renderer, recentArtifacts.Renderer))
+
+class TestRecentArtifactsRenderer(PlonepmTestCase):
+
+    def renderer(self, context=None, request=None, view=None, manager=None,
+                 assignment=None):
+
+        context = context or self.portal
+        request = request or self.app.REQUEST
+        view = view or self.portal.restrictedTraverse('@@plone')
+        manager = getUtility(IPortletManager, name='plone.leftcolumn',
+                             context=self.portal)
+        assignment = recentArtifacts.Assignment()
+
+        return getMultiAdapter((context, request, view, manager, assignment),
+                               IPortletRenderer)
+
+    def testArtifacts(self):
+
+        self.portal.invokeFactory('PPMProject', 'project1')
+        project = getattr(self.portal, 'project1')
+        project.invokeFactory('PPMStory', 'story1')
+        story1 = getattr(project, 'story1')
+        project.invokeFactory('PPMFuncReq', 'funcReq1')
+        funcReq1 = getattr(project, 'funcReq1')
+        project.invokeFactory('PPMIteration', 'iter1')
+        iter1 = getattr(project, 'iter1')
+        project.invokeFactory('PPMSysReq', 'sysReq1')
+        sysReq1 = getattr(project, 'sysReq1')
+
+        renderer = self.renderer(context=project)
+        artifacts = renderer.artifacts()
+        self.assertEquals(len(artifacts), 4)
+        self.assertEquals(artifacts[0]['url'], sysReq1.absolute_url())
+        self.assertEquals(artifacts[0]['title'], 'sysReq1')
+        self.assertEquals(artifacts[2]['url'], funcReq1.absolute_url())
+        self.assertEquals(artifacts[2]['title'], 'funcReq1')
+
+        renderer = self.renderer(context=story1)
+        artifacts = renderer.artifacts()
+        self.assertEquals(len(artifacts), 4)
+        self.assertEquals(artifacts[1]['url'], iter1.absolute_url())
+        self.assertEquals(artifacts[1]['title'], 'iter1')
+        self.assertEquals(artifacts[3]['url'], story1.absolute_url())
+        self.assertEquals(artifacts[3]['title'], 'story1')
+
 def test_suite():
 
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestProjectSimpleNavPortlet))
     suite.addTest(unittest.makeSuite(TestProjectSimpleNavRenderer))
+    suite.addTest(unittest.makeSuite(TestRecentArtifactsPortlet))
 
     return suite

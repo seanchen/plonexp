cae7b43414eb42aebc036d3d7fd77302e8d3c877
Sean Chen <sean.chen@leocorn.com>
2009-11-13 15:37:56 +0000

    adding progress status, iteration link, colleague sories list to stroy facts portlet.

--- iscorpio.plonepm/trunk/iscorpio/plonepm/tests/testStoryPortlets.py
+++ iscorpio.plonepm/trunk/iscorpio/plonepm/tests/testStoryPortlets.py
@@ -59,12 +59,59 @@ class TestStoryFactsRenderer(PlonepmPortletTestCase):
         self.portal.invokeFactory('PPMProject', 'project1')
         project1 = getattr(self.portal, 'project1')
 
-        project1.invokeFactory('PPMStory', 'story1')
+        project1.invokeFactory('PPMIteration', 'iter1', title='Iteration Title')
+        iter1 = getattr(project1, 'iter1')
+
+        project1.invokeFactory('PPMStory', 'story1', title="Story Title")
         story1 = getattr(project1, 'story1')
+        story1.xppm_iteration = 'iter1'
         renderer = self.renderer(context=story1,
                                  assignment=storyFacts.Assignment())
         info = renderer.storyInfo()
         self.assertEquals(info['url'], story1.absolute_url())
+        self.assertEquals(info['title'], 'Story Title')
+        self.assertEquals(info['iterationTitle'], 'Iteration Title')
+        self.assertEquals(info['iterationUrl'], iter1.absolute_url())
+        self.assertEquals(info['iterationIcon'], iter1.getIcon())
+
+    def testColleagueStories(self):
+
+        # we don't need this, the profile will be imported when we install
+        # the product.
+        #setup_tool = getattr(self.portal, 'portal_setup')
+        #setup_tool.runAllImportStepsFromProfile('profile-%s' % \
+        #                                        'iscorpio.plonepm:default')
+
+        self.portal.invokeFactory('PPMProject', 'project1')
+        project1 = getattr(self.portal, 'project1')
+
+        project1.invokeFactory('PPMIteration', 'iter1', title='Iteration Title')
+        iter1 = getattr(project1, 'iter1')
+
+        project1.invokeFactory('PPMStory', 'story1')
+        story1 = getattr(project1, 'story1')
+        story1.xppm_iteration = 'iter1'
+        # re-index the object is required! since this is not a default index
+        # field.
+        self.portal.portal_catalog.indexObject(story1)
+
+        project1.invokeFactory('PPMStory', 'story2')
+        story2 = getattr(project1, 'story2')
+        story2.xppm_iteration = 'iter1'
+        self.portal.portal_catalog.indexObject(story2)
+
+        project1.invokeFactory('PPMStory', 'story3')
+        story3 = getattr(project1, 'story3')
+        story3.xppm_iteration = 'iter1'
+        self.portal.portal_catalog.indexObject(story3)
+
+        renderer = self.renderer(context=story2,
+                                 assignment=storyFacts.Assignment())
+        stories = renderer.colleagueStories()
+        self.assertEquals(len(stories), 3)
+        self.assertEquals(stories[0]['url'], story1.absolute_url())
+        self.assertEquals(stories[1]['title'], story2.id)
+        self.assertEquals(stories[2]['url'], story3.absolute_url())
 
 def test_suite():
 

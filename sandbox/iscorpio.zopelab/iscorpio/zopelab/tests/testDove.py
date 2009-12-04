690d0f8832559c0c6ee15ba0905284d0e610101a
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2009-12-04 18:01:00 +0000

    testing portal_properties: 1. adding a new Plone property sheet by using propertiestool.xml 2. access the properties within it 3. change the properties within it

--- sandbox/iscorpio.zopelab/iscorpio/zopelab/tests/testDove.py
+++ sandbox/iscorpio.zopelab/iscorpio/zopelab/tests/testDove.py
@@ -27,6 +27,16 @@ class DoveTestCase(IscorpioZopelabTestCase):
         self.assertEquals(properties.getProperty('name'), 'navtree')
         self.assertTrue(properties.getProperty('something_new'))
 
+        # the testing properties.
+        properties = self.portal.portal_properties.testing_properties
+        self.assertTrue(properties.getProperty('a_boolean'))
+        self.assertEquals(properties.getProperty('lines_list'),
+                          ('first line', 'second line'))
+
+        # testing change properties.
+        properties.manage_changeProperties(a_boolean=False)
+        self.assertFalse(properties.getProperty('a_boolean'))
+
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(DoveTestCase))

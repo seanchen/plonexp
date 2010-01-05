d60d7c52739b580cc00904d0de5a74c032fbd773
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2010-01-05 16:49:52 +0000

    initial import for Leocornus Classic Theme

---
+++ themes/leocornus.themes.classic/leocornus/themes/classic/README.txt
@@ -0,0 +1,5 @@
+
+Leocornus Classic Theme
+=======================
+
+
---
+++ themes/leocornus.themes.classic/leocornus/themes/classic/__init__.py
@@ -0,0 +1,11 @@
+
+# __init__.py
+
+__author__ = "Sean Chen"
+__email__ = "sean.chen@leocorn.com"
+
+# initializing...
+def initialize(context):
+    """
+    For Zope 2 products using Zope 3 features.
+    """

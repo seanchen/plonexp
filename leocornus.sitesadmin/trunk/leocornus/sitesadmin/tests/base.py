8c85afa498b667c69e90714bac96176f0ac6b30c
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2010-02-03 20:15:22 +0000

    getting start. create a property sheet for sites admin set up basic unit test code.

---
+++ leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/base.py
@@ -0,0 +1,54 @@
+
+# base.py
+
+"""
+The base unit test cases for sites admin
+"""
+
+from Testing import ZopeTestCase
+
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import onsetup
+
+import leocornus.sitesadmin
+
+__author__ = "Sean Chen"
+__email__ = "sean.chen@leocorn.com"
+
+# 
+
+@onsetup
+def setup_product():
+    """
+    we need install our product so the testing zope server know it.
+    """
+
+    fiveconfigure.debug_mode = True
+    zcml.load_config('configure.zcml', leocornus.sitesadmin)
+    ZopeTestCase.installPackage('leocornus.sitesadmin')
+
+setup_product()
+# we need a Plone site for some of the module.
+PloneTestCase.setupPloneSite()
+
+# base test case for our product.
+class SitesAdminTestCase(PloneTestCase.PloneTestCase):
+    """
+    General steps for all test cases.
+    """
+
+    def afterSetUp(self):
+
+        self.loginAsPortalOwner()
+
+class SitesAdminFunctionalTestCase(PloneTestCase.FunctionalTestCase):
+    """
+    base test case class for functional test case.
+    """
+
+    def afterSetUp(self):
+
+        self.loginAsPortalOwner()

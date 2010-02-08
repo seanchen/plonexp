8cafa30ab4fe7a703174922d7be524b1d772f8e3
Sean Chen <sean.chen@leocorn.com>
2010-02-08 13:44:35 +0000

    tag the release 0.1: 1. add sites admin property sheet in portal_properties 2. add a configlet in plone control panel for sitesadmin_properties 3. add an utility to hide un-necessary products.

---
+++ leocornus.sitesadmin/tags/0.1/leocornus/sitesadmin/tests/testUtils.py
@@ -0,0 +1,41 @@
+
+# testUtils.py
+
+"""
+testing utilities.
+"""
+
+import unittest
+
+from base import SitesAdminTestCase
+
+__author__ = "Sean Chen"
+__email__ = "sean.chen@leocorn.com"
+
+class PropertiesTestCase(SitesAdminTestCase):
+
+    # test setting properties in properties.xml
+    def testProperties(self):
+
+        # import the iscorpio.plonelab default profiles
+        setup_tool = getattr(self.portal, 'portal_setup')
+        setup_tool.\
+            runAllImportStepsFromProfile('profile-%s' % \
+                                         'leocornus.sitesadmin:default')
+
+        # the testing properties.
+        properties = self.portal.portal_properties.sitesadmin_properties
+        self.assertEquals(properties.getProperty('title'),
+                          'Sites Admin Properties')
+        self.assertEquals(properties.getProperty('productsNotToList'),
+                          ('Products.NuPlone', 'NuPlone'))
+
+        # testing change properties.
+        properties.manage_changeProperties(title='Changed Testing Property')
+        self.assertEquals(properties.getProperty('title'),
+                          'Changed Testing Property')
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(PropertiesTestCase))
+    return suite

f60b19737a2f3aeb1a3ff7a3affcc4e176f92c52
Sean Chen <sean.chen@leocorn.com>
2010-02-09 19:36:15 +0000

    adding setuphandler to make sure install membrane when we install sitesadmin. adding for test cases.  try to create extra Plone sites BUT it does NOT work!

--- leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/testUtils.py
+++ leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/testUtils.py
@@ -7,10 +7,37 @@ testing utilities.
 
 import unittest
 
+from Products.PloneTestCase.setup import SiteSetup
+
 from base import SitesAdminTestCase
 
 __author__ = "Sean Chen"
 __email__ = "sean.chen@leocorn.com"
+
+class InstallationTestCase(SitesAdminTestCase):
+    """
+    make sure we can install sitesadmin properly.
+    """
+
+    def testInstallation(self):
+        """
+        the default Plone site for testing have everything installed
+        properly, we will create a new Plone site here for testing.
+        """
+
+        # setup an empty Plone site.
+        import pdb; pdb.set_trace()
+        SiteSetup(id='installation',
+                  policy='Default Plone',
+                  products=['leocornus.sitesadmin'],
+                  quiet=0,
+                  with_default_memberarea=1,
+                  base_profile='CMFPlone:plone',
+                  extension_profiles=()).run()
+        install = getattr(self.app, 'installation')
+        # by this point, membrane should be installed.
+        user_folder = install.acl_users
+        assertTrue('membrane_users' in user_folder.objectIds())
 
 class PropertiesTestCase(SitesAdminTestCase):
 
@@ -37,5 +64,8 @@ class PropertiesTestCase(SitesAdminTestCase):
 
 def test_suite():
     suite = unittest.TestSuite()
+    # could not create another Plone site! Maybe we need create a didderent
+    # layer!
+    #suite.addTest(unittest.makeSuite(InstallationTestCase))
     suite.addTest(unittest.makeSuite(PropertiesTestCase))
     return suite

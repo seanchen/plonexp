13742a5d938f1d617df909a2bcf399414ec7fe89
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2010-02-17 19:54:44 +0000

    added a sites field to save how user associate with sites, what sites that this user has member role and what group, role that this user been assigned for each sites.

--- leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/testUserAccount.py
+++ leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/testUserAccount.py
@@ -9,6 +9,8 @@ import unittest
 
 from Products.ATContentTypes.tests.atcttestcase import ATCTTypeTestCase
 
+from Products.CMFCore.utils import getToolByName
+
 from leocornus.sitesadmin.content.user import UserAccount
 from base import SitesAdminTestCase
 
@@ -38,7 +40,12 @@ class UserAccountTest(SitesAdminTestCase):
         self.portal.invokeFactory('UserAccount', 'user1')
         user1 = getattr(self.portal, 'user1')
         user1.setUserName("user1test")
+        user1.setFullname("User Full Name")
         user1.setPassword('user1password')
+        user1.setEmail('sean.chen@ontario.ca')
+        sites = ("{'id':'gsdc/sso_test', 'roles':['Member','Reviewer'], 'groups':['testing','contributor']}",
+                 "{'id':'gsdc/cts', 'roles':['Reviewer'], 'groups':['contributor']}")
+        user1.setSites(sites)
         self.portal.membrane_tool.indexObject(user1)
 
         credentials = {'login' : 'user1test',
@@ -46,6 +53,13 @@ class UserAccountTest(SitesAdminTestCase):
         credit = self.portal.acl_users.membrane_users.authenticateCredentials(credentials)
         self.failIf(credit is None)
         self.assertTrue('user1test' in credit)
+
+        # testing the membership info.
+        mTool = getToolByName(self.portal, 'portal_membership')
+        theMember = mTool.getMemberById('user1test')
+        self.assertEquals('User Full Name', theMember.getProperty('fullname'))
+        self.assertEquals('sean.chen@ontario.ca', theMember.getProperty('email'))
+        self.assertEquals(sites, theMember.getProperty("sites"))
 
 tests.append(UserAccountTest)
 

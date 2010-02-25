ae99b3e5517e8b69c859d0346b8c1cbd4b2e9b41
Sean Chen <sean.chen@leocorn.com>
2010-02-25 16:41:04 +0000

    change to use quick installer to install membrane if it is not installed.  test cases are updated to have a separate empty Plone site within the same Zope server for testing installation.

---
+++ leocornus.sitesadmin/trunk/leocornus/sitesadmin/tests/testImportSteps.py
@@ -0,0 +1,57 @@
+
+# testImportSteps.py
+
+"""
+testing generic setup import steps.
+"""
+
+import unittest
+
+from Products.CMFCore.utils import getToolByName
+
+from base import SitesAdminTestCase
+
+__author__ = "Sean Chen"
+__email__ = "sean.chen@leocorn.com"
+
+class InstallationTestCase(SitesAdminTestCase):
+    """
+    make sure we can install sitesadmin properly.
+    """
+
+    def afterSetUp(self):
+
+        # get an empty Plone site to test the generic setup installation
+        self.emptySite = getattr(self.app, 'test_install')
+
+    def testQuickInstaller(self):
+        """
+        install leocornus.sitesadmin by using the quick installer.
+        """
+
+        user_folder = self.emptySite.acl_users
+        self.failIf('membrane_users' in user_folder.objectIds())
+
+        installer = getToolByName(self.emptySite, 'portal_quickinstaller')
+        installer.installProduct('leocornus.sitesadmin')
+        # by this point, membrane should be installed.
+        self.assertTrue('membrane_users' in user_folder.objectIds())
+
+    def testImportProfile(self):
+        """
+        test using setup tool to import profiles.
+        """
+
+        user_folder = self.emptySite.acl_users
+        self.failIf('membrane_users' in user_folder.objectIds())
+
+        setup_tool = getattr(self.emptySite, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile('profile-%s' % \
+                                                'leocornus.sitesadmin:default')
+
+        self.assertTrue('membrane_users' in user_folder.objectIds())
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(InstallationTestCase))
+    return suite

57937c9411f0c78b0d45a6855d069c9fe16f1d1a
Sean Chen <sean.chen@leocorn.com>
2010-04-15 16:45:47 +0000

    preparing the initial release 0.1

--- themes/iscorpio.themes.redmaple/setup.py
+++ themes/iscorpio.themes.redmaple/setup.py
@@ -10,21 +10,25 @@ __email__ = "sean.chen@leocorn.com"
 theFolder = "iscorpio/themes/redmaple"
 
 setup(name='iscorpio.themes.redmaple',
-      version=open(os.path.join(theFolder, "version.txt")).read(),
+      version=open(os.path.join(theFolder, "version.txt")).read().split('\n')[0],
       description="iscorpio readmaple Plone 3 theme",
       long_description=open(os.path.join(theFolder, "README.txt")).read() +
                        "\n" +
                        open(os.path.join(theFolder, "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules :: Theme",
+        "Programming Language :: Zope",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='Zope Plone Python Theme Web',
       author='Sean Chen',
       author_email='sean.chen@leocorn.com',
-      url='http://plonexp.svn.sourceforge.net/svnroot/plonexp/themes/iscorpio.themes.redmaple',
+      url='http://plonexp.leocorn.com/plonepm',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['iscorpio'],

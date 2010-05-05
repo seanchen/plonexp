e52fefac9c82dc5c253777f695733f266c8ad263
iscorpio <iscorpio@c1d1c834-8093-458a-9708-2531eee3f3b2>
2010-05-05 15:48:57 +0000

    the classic global section is ready: - create a CMF Action Category classic site actions for links on the global   setcion - using the marquee for scrolling the lastes comming event! only working well   in IE! - updateed the css correpondently

--- themes/leocornus.themes.classic/leocornus/themes/classic/layout/header.py
+++ themes/leocornus.themes.classic/leocornus/themes/classic/layout/header.py
@@ -5,9 +5,19 @@
 viewlets in portal header for the Leocornus classic theme.
 """
 
+from DateTime.DateTime import DateTime
+
+from zope.component import getMultiAdapter
+
+from Acquisition import aq_inner
+
+from plone.memoize.instance import memoize
+
 from plone.app.layout.viewlets.common import ViewletBase
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFCore.utils import getToolByName
 
 __author__ = "Sean Chen"
 __email__ = "sean.chen@leocorn.com"
@@ -32,4 +42,21 @@ class GlobalSectionViewlet(ViewletBase):
 
     def update(self):
 
-        super(GlobalSectionViewlet, self).update()
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        self.classic_site_actions = context_state.actions().get('classic_site_actions', None)
+
+    @memoize
+    def latestEvent(self):
+
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        limit = 1
+        # TODO: this should be configurable some where!
+        state = 'published'
+        return catalog(portal_type='Event',                                                            
+                       review_state=state,
+                       end={'query': DateTime(),                                                       
+                            'range': 'min'},                                                           
+                       sort_on='start',
+                       sort_limit=limit)[:limit]

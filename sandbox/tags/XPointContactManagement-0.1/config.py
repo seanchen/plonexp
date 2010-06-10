88fd63fc4c5fa0c36d601b766f4de592a4751bb8
Sean Chen <sean.chen@leocorn.com>
2010-06-10 17:49:15 +0000

    backup the legacy contact management code.

---
+++ sandbox/tags/XPointContactManagement-0.1/config.py
@@ -0,0 +1,14 @@
+# config.py
+
+# configuration constants for XPoint Consulting Contact Management, a
+# contact could by a client, a potential client, etc.
+
+# project name.
+PROJECTNAME = "XPointContactManagement"
+
+# skins directory for this project, contains gif, vie pt ...
+SKINS_DIR = "skins"
+
+# A global name space when this product is loaded into Python is 
+# preserved. It passes to the package_home() etc.
+GLOBALS = globals()
